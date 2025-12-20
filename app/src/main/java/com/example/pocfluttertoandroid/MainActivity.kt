b1da70dee94c733b4104bbd0d30f4c9473b51620
Omkar <omkarsawant@Omkars-MacBook-Pro.local>
2025-12-20 15:13:21 +0530

    - Method channel bridge added

--- app/src/main/java/com/example/pocfluttertoandroid/MainActivity.kt
+++ app/src/main/java/com/example/pocfluttertoandroid/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.pocfluttertoandroid
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.pocfluttertoandroid.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding  : ActivityMainBinding
     lateinit var flutterEngine : FlutterEngine
+    lateinit var mSenderChannel : MethodChannel
+    lateinit var mReceiverChannel : MethodChannel
     companion object{
         const val ENGINE_ID = "12345"
     }
@@ -37,6 +42,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun onClickListeners() {
         binding.btnNavigate.setOnClickListener {
+            mSenderChannel = MethodChannel(
+                flutterEngine
+                    .dartExecutor
+                    .binaryMessenger,
+                "native_args"
+            )
+
+            val data: MutableMap<String?, Any?> = HashMap()
+            data.put("platFormName", "Android Native")
+            data.put("userId", "42")
+            mSenderChannel.invokeMethod("initialScreen" , data)
+
             startActivity(
                 FlutterActivity
                     .withCachedEngine(ENGINE_ID)
@@ -58,5 +75,24 @@ class MainActivity : AppCompatActivity() {
         FlutterEngineCache
             .getInstance()
             .put(ENGINE_ID, flutterEngine)
+
+        mReceiverChannel = MethodChannel(
+            flutterEngine
+                .dartExecutor
+                .binaryMessenger,
+            "flutter_navigation"
+        )
+
+        mReceiverChannel.setMethodCallHandler { call, result ->
+            if (call.method.equals("close")) {
+                val valueFromFlutter = call.arguments as HashMap<String?, String?>
+                var mData = ""
+                valueFromFlutter.forEach {
+                    mData += it.key?.uppercase()+" : "+it.value+"\n"
+                }
+                binding.txtData.text = mData
+                //onBackPressedDispatcher.onBackPressed()
+            }
+        }
     }
 }
